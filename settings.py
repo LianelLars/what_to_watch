095d2c4143ac71f182dea0ea39ddd16aeba26496
LianelLars <lianel.larson@gmail.com>
2023-12-25 14:47:37 +0300

    Add API views.

--- settings.py
+++ settings.py
@@ -3,5 +3,6 @@ from os import getenv
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URI')
+    JSON_AS_ASCII = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = getenv('SECRET_KEY')
